b30640d8a7f0317c9149defcf56b003932249909
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-02-17 12:04:31 +0900

    v1.0.1 : タスク表示機能(一覧、ID指定)追加

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,17 +1,33 @@
 using Microsoft.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+/*
+WebAPI 基本構成のメモ
+①builder の DIコンテナへサービス登録(Addxxx)
+②登録したDI(サービス)を使って動作するミドルウェア(Usexxx群)を構成・実行
+③app.Mapxxxでルーティングの定義（エンドポイントや処理の定義）をする。
+*/
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+/* DI準備   */
+var builder = WebApplication.CreateBuilder(args); // DIを含む、ビルダーオブジェクトの作成
+var connectionString = builder.Configuration.GetConnectionString("Tasks") ?? "Data Source=Tasks.db"; // SQLiteに接続するための接続文字列を取得
+
+builder.Services.AddEndpointsApiExplorer();  // エンドポイントに関するのDIコンテナ登録
+
+builder.Services.AddSqlite<TasksDb>(connectionString);
+builder.Services.AddSwaggerGen(c => // Swaggerのドキュメント生成関連DIコンテナ登録
 {
-     c.SwaggerDoc("v1", new OpenApiInfo {
-         Title = "TaskManager API",
-         Description = "Making the your tasks!",
-         Version = "v1" });
+   c.SwaggerDoc("v1", new OpenApiInfo
+   {
+      Title = "TaskManager API",
+      Description = "Making the your tasks!",
+      Version = "v1"
+   });
 });
 
-var app = builder.Build();
+/* ミドルウェア構築  */
+var app = builder.Build(); // WebApplicationのインスタンス生成
 if (app.Environment.IsDevelopment())
 {
    app.UseSwagger();
@@ -21,6 +37,27 @@ if (app.Environment.IsDevelopment())
    });
 }
 
-app.MapGet("/", () => "Welcome to TaskManager! (^o^)b\n" + "Please access to [ http://localhost:(portNo)/swagger ]" );
+/* ルーティング   */
+app.MapGet("/", () => "Welcome to TaskManager! (^o^)b\n" + "Please access to [ http://localhost:(portNo)/swagger ]");
+
+// タスク一覧表示
+app.MapGet("/tasks", async (TasksDb db) => await db.Tasks.ToListAsync());
+
+// タスク表示(ID指定)
+app.MapGet("/tasks{id}", async (TasksDb db, int id) =>
+{
+   await db.Tasks.ToListAsync();
+   var task = await db.Tasks.FindAsync(id);
+   return task is not null ? Results.Ok(task) : Results.NotFound();
+});
+
+// タスク追加
+//TODO: MapPost "/tasks"
+
+// タスク更新
+//TODO: MapPut "/tasks{id}"
+
+// タスク削除
+//TODO: MapDelete "/tasks{id}"
 
 app.Run();
--- TaskManager/Tasks.cs
+++ TaskManager/Tasks.cs
@@ -1,10 +1,20 @@
+using Microsoft.EntityFrameworkCore;
 namespace TaskManager.Models
 {
+    // タスク構成基本情報
     public class Tasks
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public DateTime CreateDate { get; set; }
-        public DateTime? Update { get; set; }
+        public string? Details { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+
+    // タスク管理DB
+    class TasksDb : DbContext
+    {
+        public TasksDb(DbContextOptions options) : base(options) { }
+        public DbSet<Tasks> Tasks { get; set; } = null!;
     }
 }
